fe4976e876f226a34c93f8bc816e5e01ef2dfa25
dung <dad@abc.c>
2025-03-11 00:58:22 +1100

    save change

--- bash_scripts/pretrain.sh
+++ bash_scripts/pretrain.sh
@@ -7,7 +7,7 @@ export NCCL_IB_DISABLE=1
 
 basefolder="/hdd/dungda/LM/"
 
-cmd="torchrun --nnodes=1 --nproc_per_node=1 --master_port=25001 \
+cmd="torchrun --nnodes=1 --nproc_per_node=4 --master_port=25001 \
     llava/train/train_mem.py \
     --model_name_or_path meta-llama/Llama-3.2-1B \
     --data_path ${basefolder}/data/instruct/matched_instruct.json \
@@ -17,7 +17,7 @@ cmd="torchrun --nnodes=1 --nproc_per_node=1 --master_port=25001 \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end \
     --bf16 True \
-    --output_dir ${basefolder}/checkpoints/llava-med-1b-pretrain_20k/ \
+    --output_dir ${basefolder}/checkpoints/llava-med-1b-pretrain_20k_lr4e3/ \
     --num_train_epochs 1 \
     --per_device_train_batch_size 2 \
     --per_device_eval_batch_size 4 \
@@ -26,7 +26,7 @@ cmd="torchrun --nnodes=1 --nproc_per_node=1 --master_port=25001 \
     --save_strategy "steps" \
     --save_steps 2400 \
     --save_total_limit 1 \
-    --learning_rate 2e-3 \
+    --learning_rate 4e-3 \
     --weight_decay 0. \
     --warmup_ratio 0.03 \
     --lr_scheduler_type "cosine" \
--- bash_scripts/train_lora2.sh
+++ bash_scripts/train_lora2.sh
@@ -22,16 +22,20 @@ mm_vision_select_layer="-1"
 # python llava/train/train.py --model_name_or_path ${model_name_or_path} --data_path ${data_path} --image_folder ${image_folder} --tune_mm_mlp_adapter True --output_dir ${output_dir} --vision_tower ${vision_tower} --mm_vision_select_layer -2 --mm_use_im_start_end True --bf16 True --num_train_epochs 1 --per_device_train_batch_size 1 --per_device_eval_batch_size 1 --gradient_accumulation_steps ${accumulation_steps} --evaluation_strategy "no" --save_strategy "steps" --save_steps 1000 --save_total_limit 3 --learning_rate 2e-3 --weight_decay 0. --warmup_ratio 0.03 --lr_scheduler_type "cosine" --logging_steps 1 --tf32 True --model_max_length 1024 --lazy_preprocess True --gradient_checkpointing True --dataloader_num_workers 8 --report_to wandb
 
 ############### Option 2 to run code with multi-GPU ###############
-ranks=("8" "16" )
+# ranks=("8" "16" )
+ranks=("8")
+alphas=("16" "64")
 
 for rank in "${ranks[@]}"; do
-output_dir=${basefolder}/checkpoints/llava-med-1b-train-lora-rank${rank}-alpha32-dr0.01/
+for alpha in "${alphas[@]}"; do
+output_dir=${basefolder}/checkpoints/llava-med-1b-train-lora-rank${rank}-alpha${alpha}-dr0.01/
 cmd="torchrun --nnodes=1 --nproc_per_node=${GPUS} --master_port=25001 llava/train/train_mem.py --model_name_or_path ${model_name_or_path} \
 --data_path ${data_path} --image_folder ${image_folder} --tune_mm_mlp_adapter True --output_dir ${output_dir} --vision_tower ${vision_tower} \
 --mm_vision_select_layer ${mm_vision_select_layer} --mm_use_im_start_end True --bf16 True --num_train_epochs 2 --per_device_train_batch_size 2 \
 --per_device_eval_batch_size 3 --gradient_accumulation_steps ${accumulation_steps} --evaluation_strategy "no" --save_strategy "steps"\
  --save_steps 1000 --save_total_limit 3 --learning_rate 4e-3 --weight_decay 0.0 --warmup_ratio 0.03 --lr_scheduler_type "cosine" --logging_steps 1 --tf32 True --model_max_length 1024 \
-  --lazy_preprocess True --gradient_checkpointing True --dataloader_num_workers 8 --report_to none --cache_dir ${basefolder}/Llama-3.2-1B/.cache/ --tokenizer_1B True --lora True --lora_r ${rank} --lora_alpha 32 --lora_dropout 0.01"
+  --lazy_preprocess True --gradient_checkpointing True --dataloader_num_workers 8 --report_to none --cache_dir ${basefolder}/Llama-3.2-1B/.cache/ --tokenizer_1B True --lora True --lora_r ${rank} --lora_alpha ${alpha} --lora_dropout 0.01"
 echo ${cmd}
 eval ${cmd}
+done
 done
