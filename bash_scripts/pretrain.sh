2221420c9d090afe57b0140d13a6265baa3a0c93
dung <dad@abc.c>
2025-03-10 16:40:59 +1100

    save change

--- bash_scripts/pretrain.sh
+++ bash_scripts/pretrain.sh
@@ -10,14 +10,14 @@ basefolder="/hdd/dungda/LM/"
 cmd="torchrun --nnodes=1 --nproc_per_node=1 --master_port=25001 \
     llava/train/train_mem.py \
     --model_name_or_path meta-llama/Llama-3.2-1B \
-    --data_path ${basefolder}/data/instruct/matched_instruct_50.json \
+    --data_path ${basefolder}/data/instruct/matched_instruct.json \
     --image_folder ${basefolder}/data/images \
     --vision_tower  openai/clip-vit-base-patch16 \
     --tune_mm_mlp_adapter True \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end \
     --bf16 True \
-    --output_dir ${basefolder}/checkpoints/llava-med-1b-pretrain \
+    --output_dir ${basefolder}/checkpoints/llava-med-1b-pretrain_20k/ \
     --num_train_epochs 1 \
     --per_device_train_batch_size 2 \
     --per_device_eval_batch_size 4 \
@@ -28,7 +28,7 @@ cmd="torchrun --nnodes=1 --nproc_per_node=1 --master_port=25001 \
     --save_total_limit 1 \
     --learning_rate 2e-3 \
     --weight_decay 0. \
-    --warmup_ratio 0.0 \
+    --warmup_ratio 0.03 \
     --lr_scheduler_type "cosine" \
     --logging_steps 1 \
     --tf32 True \
